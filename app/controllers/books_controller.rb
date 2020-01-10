c67c1254e037b68bd99bdb3a2eb72338dd3a107a
mwasswa <m_wasswa@yahoo.com>
2020-01-10 16:50:01 -0500

    Fixing error in controller
    Fixed error in with delete action to make sure access is properly
    restricted.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,27 @@
-class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :update, :destroy]
+class BooksController < ProtectedController
+  # use ProtectedController so that the user has to sign in to access app features
+  before_action :set_book, only: %i[show]
 
   # GET /books
   def index
-    @books = Book.all
-
+    # @books = Book.all
+    @books = current_user.books.all
+    # only an authenticated user should be able to see a book.
     render json: @books
   end
 
   # GET /books/1
   def show
+    @book = current_user.book
     render json: @book
   end
 
   # POST /books
   def create
-    @book = Book.new(book_params)
+    # @book = Book.new(book_params)
+    @book = current_user.books.build(books_params)
+    # only an authenticated user should be able to create a new book within
+    # their account.
 
     if @book.save
       render json: @book, status: :created, location: @book
@@ -26,6 +32,8 @@ class BooksController < ApplicationController
 
   # PATCH/PUT /books/1
   def update
+    @book = current_user.books.find(params[:id])
+    # only the signed in and current_user is able to update a book
     if @book.update(book_params)
       render json: @book
     else
@@ -35,11 +43,12 @@ class BooksController < ApplicationController
 
   # DELETE /books/1
   def destroy
+    @book = current_user.books.find(params[:id])
+    # only the signed in and current_user is able to delete a book
     @book.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
     end
